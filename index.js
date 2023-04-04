36b22fee2398fa5549aa478ea1f5ce4a6d962b6f
MUHAMMED YAYLA <muhammeddyayla@gmail.com>
2023-04-04 11:28:54 +0300

    informations change

--- index.js
+++ index.js
@@ -44,3 +44,19 @@ function scrollIntoFrequently_Asked_Questions() {
   });
 }
 
+function geoLocation() {
+  var anchor = document.createElement('a');
+  anchor.href = 'https://www.google.com/maps/place/London,+UK/@51.5285262,-0.2664016,11z/data=!3m1!4b1!4m6!3m5!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862!16zL20vMDRqcGw';
+  anchor.target = "_blank";
+  anchor.click();
+}
+
+function gowhatsapp() {
+  var anchor = document.createElement('a');
+  anchor.href = 'https://wa.me/447812814703';
+  anchor.target = "_blank";
+  anchor.click();
+}
+function goMail() {
+  window.location.href = "mailto:Duhan27t@aol.com";
+}
